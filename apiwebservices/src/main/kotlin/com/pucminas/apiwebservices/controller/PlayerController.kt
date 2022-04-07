8ddb4daead8c240b632004718cf299dddd19e246
vitor batista <vitor.shofar@gmail.com>
2022-04-07 11:30:02 -0300

    get players

--- apiwebservices/src/main/kotlin/com/pucminas/apiwebservices/controller/PlayerController.kt
+++ apiwebservices/src/main/kotlin/com/pucminas/apiwebservices/controller/PlayerController.kt
@@ -1,9 +1,28 @@
 package com.pucminas.apiwebservices.controller
 
 import com.pucminas.apiwebservices.service.PlayerService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class PlayerController(
     private val playerService: PlayerService
-){}
+){
+    @GetMapping("/players")
+    fun getClub(): ResponseEntity<*> {
+        return ResponseEntity.ok(playerService.getPLayers())
+    }
+
+    @GetMapping("/players/{id}")
+    fun getClub(@PathVariable(value = "id") playerId: Long): ResponseEntity<*> {
+        val player = playerService.getPlayer(playerId)
+
+        return if(player == null)
+            ResponseEntity<Any>(HttpStatus.NOT_FOUND)
+        else
+            ResponseEntity.ok(player)
+    }
+}
--- apiwebservices/src/main/kotlin/com/pucminas/apiwebservices/service/PlayerService.kt
+++ apiwebservices/src/main/kotlin/com/pucminas/apiwebservices/service/PlayerService.kt
@@ -1,9 +1,24 @@
 package com.pucminas.apiwebservices.service
 
+import com.pucminas.apiwebservices.model.Player
 import com.pucminas.apiwebservices.repository.PlayerRepository
 import org.springframework.stereotype.Service
 
 @Service
 class PlayerService(
     private val playerRepository: PlayerRepository
-) {}
+) {
+
+    fun getPLayers(): List<Player> {
+        return playerRepository.findAll()
+    }
+
+    fun getPlayer(playerId: Long): Player? {
+        val search = playerRepository.findById(playerId)
+
+        return if(search.isPresent)
+            search.get()
+        else
+            null
+    }
+}
